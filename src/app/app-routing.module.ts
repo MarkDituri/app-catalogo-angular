165e339becc9757fdffa05a971a93dd558642446
MarkDituri <marcos.e.dituri@gmail.com>
2021-10-06 23:49:30 -0300

    enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { ItemComponent } from './item/item.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  { path: 'home', component: CatalogoComponent},
+  { path: 'about', component: ItemComponent},
+  { path: 'item', component: ItemComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+ 
+  imports: [
+    RouterModule.forRoot(routes, {useHash: true})
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NgModule } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CatalogoComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
