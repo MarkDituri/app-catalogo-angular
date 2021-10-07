2404ee234a169c586bd4e143d93deab9c8960dcd
MarkDituri <marcos.e.dituri@gmail.com>
2021-10-07 17:12:05 -0300

    interface y servicio producto

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductosService } from 'src/app/services/productos.service';
+
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogoComponent implements OnInit {
 
-  constructor() { }
+  constructor( public productosService: ProductosService) { }
 
   ngOnInit(): void {
   }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { Injectable } from '@angular/core';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProductosService {
+  cargando = true;
+  producto:Producto[] = [];
+
+  constructor( private http: HttpClient ) {
+    this.cargarProductos();
+  }
+
+  private cargarProductos() {
+    this.http.get('https://angular-catalogo-98f71-default-rtdb.firebaseio.com/productos_idx.json')
+    .subscribe( (resp: Producto) => {
+      this.cargando = false;
+      this.producto = resp;
+    });
+  }
+} // Fin ProductosService
